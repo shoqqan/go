e9393b38b02f4535522b1e57646ec6e4accf6a58
shoqqan <tataev.shokan@gmail.com>
2025-01-14 14:20:47 +0500

    [fix]:routes

--- routing-gorilla-mux/todolists/todolists.go
+++ routing-gorilla-mux/todolists/todolists.go
@@ -8,25 +8,34 @@ import (
 )
 
 func createTodolist(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "todolist created\n path: %s", r.URL.Path)
+	_, err := fmt.Fprintf(w, "todolist created\n path: %s", r.URL.Path)
+	if err != nil {
+		return
+	}
 }
 
 func updateTodolist(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	fmt.Fprintf(w, "todolist with id: %s updated", id)
+	_, err := fmt.Fprintf(w, "todolist with id: %s updated", id)
+	if err != nil {
+		return
+	}
 }
 
 func InitTodolistsRouter(parentRouter *mux.Router) {
 	todolistsRouter := parentRouter.PathPrefix("/todolists").Subrouter()
 
-	todolistsRouter.HandleFunc("/", createTodolist).Methods("POST")
-	todolistsRouter.HandleFunc("/{id}", updateTodolist).Methods("PATCH")
+	todolistsRouter.HandleFunc("", createTodolist).Methods("POST").Schemes("http")
+	todolistsRouter.HandleFunc("{id}", updateTodolist).Methods("PATCH").Schemes("http")
 
-	parentRouter.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+	err := parentRouter.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 		path, _ := route.GetPathTemplate()
 		methods, _ := route.GetMethods()
 		log.Printf("Route: %s, Methods: %v", path, methods)
 		return nil
 	})
+	if err != nil {
+		return
+	}
 }
